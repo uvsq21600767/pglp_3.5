b2e40794f0f698b1492daa0a7c36b9d44f2fbe77
Thomas Cocquebert <thomas.cocquebert@gmail.com>
2020-05-22 01:01:41 +0200

    Initial state of UneClassMetier

---
+++ src/main/java/fr/uvsq/UneClasseMetier.java
@@ -0,0 +1,11 @@
+package fr.uvsq;
+
+import java.time.LocalDateTime;
+
+public class UneClasseMetier {
+
+    public void uneMethodeMetier() {
+        System.out.println(LocalDateTime.now() + ": Début de uneMethodeMetier");
+        System.out.println(LocalDateTime.now() + ": Fin de uneMethodeMetier");
+    }
+}
